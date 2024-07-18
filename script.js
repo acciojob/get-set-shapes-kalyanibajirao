545ed367ec890923bd7e3ff4878839c2c49065f5
Kalyani Bajirao <95849305+kalyanibajirao@users.noreply.github.com>
2024-07-18 20:26:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 class Rectangle {
     constructor(width, height) {
-        this.width = width;
-        this.height = height;
+        this._width = width;
+        this._height = height;
     }
 
-    getWidth() {
-        return this.width;
+    get width() {
+        return this._width;
     }
 
-    getHeight() {
-        return this.height;
+    get height() {
+        return this._height;
     }
 
     getArea() {
-        return this.width * this.height;
+        return this._width * this._height;
     }
 }
 
@@ -23,18 +23,6 @@ class Square extends Rectangle {
     }
 
     getPerimeter() {
-        return 4 * this.getWidth(); // Since it's a square, all sides are equal
+        return 4 * this._width; // or 4 * this._height since both are equal
     }
 }
-
-// Example usage
-const rectangle = new Rectangle(5, 10);
-console.log(rectangle.getWidth()); // Output: 5
-console.log(rectangle.getHeight()); // Output: 10
-console.log(rectangle.getArea()); // Output: 50
-
-const square = new Square(7);
-console.log(square.getWidth()); // Output: 7
-console.log(square.getHeight()); // Output: 7
-console.log(square.getArea()); // Output: 49
-console.log(square.getPerimeter()); // Output: 28
